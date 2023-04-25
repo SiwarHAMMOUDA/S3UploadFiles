145319a6bae183a25f369cbb68a37611f9229656
SiwarHAMMOUDA <siwar3hammouda@gmail.com>
2023-04-25 23:55:31 +0200

    Uploding files to aws s3

---
+++ src/main/java/siwar/com/S3UPDEDWFILES/S3UpdedwfilesApplication.java
@@ -0,0 +1,13 @@
+package siwar.com.S3UPDEDWFILES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3UpdedwfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3UpdedwfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/siwar/com/S3UPDEDWFILES/S3UpdedwfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package siwar.com.S3UPDEDWFILES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3UpdedwfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
